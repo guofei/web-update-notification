3de70aab615f61bba13e8e468ec09675f6788b2a
guofei <guofei1986@gmail.com>
2015-10-27 23:43:19 +0900

    add totle user count to report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,7 @@
 class ReportsController < ApplicationController
   def index
     report = {
+      'total user count': User.count,
       users: User.where(:created_at=> 1.months.ago..Time.now).group('DATE(created_at)').count
     }
 
