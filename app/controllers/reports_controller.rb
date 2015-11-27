2a86d9752f687ddb7626e011ccfffd1089d8237f
guofei <guofei1986@gmail.com>
2015-11-28 01:58:13 +0900

    report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,12 @@
 class ReportsController < ApplicationController
   def index
+    sec = 3 * 60 * 60
     report = {
       'total user count': User.count,
       'users': User.where(created_at: 1.months.ago..Time.now).group('DATE(created_at)').count,
       'total pages': Page.count,
-      'active pages': Page.where(stop_fetch: false).count
+      'active pages': Page.where(stop_fetch: false).count,
+      'paid pages': Page.where('sec < ?', sec).count
     }
 
     render json: report
