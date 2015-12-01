d08db5999999e78a1b52495b345826d67ecdc41f
guofei <guofei1986@gmail.com>
2015-12-02 02:02:27 +0900

    fix csv

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,7 +20,7 @@ class PagesController < ApplicationController
   # GET /pages/1
   # GET /pages/1.json
   def show
-    render json: @page
+    render json: @page and return
   end
 
   # POST /pages
