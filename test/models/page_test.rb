abcf0ea73ff3d587327a256568d602a1e1651dda
guofei <guofei1986@gmail.com>
2016-02-03 00:43:59 +0900

    test: page second test

--- test/models/page_test.rb
+++ test/models/page_test.rb
@@ -18,7 +18,28 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'sec 60s, updated at 4 minutes ago' do
+    page = Page.new(sec: 60, updated_at: 4.minutes.ago)
+    assert_equal(3.minutes.seconds, page.second)
+  end
+
+  test 'sec 60s, updated at 10 minutes ago' do
+    page = Page.new(sec: 60, updated_at: 10.minutes.ago)
+    assert_equal(15.minutes.seconds, page.second)
+  end
+
+  test 'sec 60s, updated at 20 minutes ago' do
+    page = Page.new(sec: 60, updated_at: 20.minutes.ago)
+    assert_equal(15.minutes.seconds, page.second)
+  end
+
+  test 'sec 4hour, updated at 20 minutes ago' do
+    page = Page.new(sec: 4.hours.seconds, updated_at: 20.minutes.ago)
+    assert_equal(4.hours.seconds, page.second)
+  end
+
+  test 'sec 4hour, updated at 1 day ago' do
+    page = Page.new(sec: 4.hours.seconds, updated_at: 2.days.ago)
+    assert_equal(1.days.seconds, page.second)
+  end
 end
